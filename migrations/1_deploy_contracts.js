d63f8384d4ccbdbb0917884c5813041b17cb5980
Giacomo Guidotto <giacomo.guidotto@gmail.com>
2024-02-05 18:07:22 +0100

    some changes

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,11 +1,11 @@
 // const ConvertLib = artifacts.require("ConvertLib");
 // const MetaCoin = artifacts.require("MetaCoin");
 const Radici = artifacts.require("Radici.sol");
-const Marketplace = artifacts.require("Marketplace.sol");
+// const Marketplace = artifacts.require("Marketplace.sol");
 
 module.exports = function(deployer) {
   // deployer.deploy(ConvertLib);
   // deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(Marketplace);
+  // deployer.deploy(Marketplace);
   deployer.deploy(Radici);
 };
